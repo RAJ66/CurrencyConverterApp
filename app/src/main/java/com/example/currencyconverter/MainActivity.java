b53aff945ca3ac9ada7eec705dc7979ae774437d
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-12-29 13:12:18 +0000

    ViewHolder implementation

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,18 +3,36 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText valor = findViewById(R.id.editValor);
+        EditText valor = findViewById(R.id.edit_value);
         valor.getText();
 
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textLibra = findViewById(R.id.text_libra);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.buttonCalculate);
+
+
+    }
+
+    private  static class ViewHolder{
+        EditText editValue;
+        TextView textDolar;
+        TextView textLibra;
+        Button buttonCalculate;
 
     }
 
